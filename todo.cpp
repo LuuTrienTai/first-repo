2079459e1beb8aab72a87d40952542af893f0475
Luu Trien Tai <luutai@192.168.1.48>
2026-01-04 12:18:14 +0700

    First commit

---
+++ todo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <fstream>
+#include <sstream>
+#include <filesystem>
+
+using namespace std;
+
+int main() {
+    cout << "hello world" << endl;
+
+    return 0;
+}
